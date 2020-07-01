8381403324f5742ca3be4e9df0d1fd2581a05e99
sunnyRK <radadiyasunny970@gmail.com>
2020-07-01 14:43:40 +0530

    Handle try catch and added check private balance feature

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import web3 from '../config/web3/web3';
 import Layout from '../components/Layout';
 import {getWitness} from "../functions/getWitness";
 import Axios from 'axios';
+const BN = require('bignumber.js');
 
 class Index extends Component {
 
@@ -13,37 +14,83 @@ class Index extends Component {
         recipient:"",
         amount: "",
         claimamount: "",
-        zkContractAddress: "0x0B6B70493d7cEf43A4E8eC84E62B9C6767b6bda0"
+        zkContractAddress: "0x0B6B70493d7cEf43A4E8eC84E62B9C6767b6bda0",
+        daiBalance: 0,
+        claimBalance: 0
     }
 
-    ontransfer = async () => {
+    checkBalalnce = async () => {
         event.preventDefault();
         try {
             const accounts = await web3.eth.getAccounts();
 
             const daiInstance = await getERCContractInstance(web3);
-            const approvehash = await daiInstance.methods.approve(
-                this.state.zkContractAddress, // ZkPrivateDai Contract address
-                this.state.claimamount
-            ).send({
-                from: accounts[0]
-            });
-            console.log("approveHash: ", approvehash);
-
-            const senderBalalnce = await daiInstance.methods.balanceOf(accounts[0]).call();
-
             const zkInstance = await getZkSnarkInstance(web3);
-            const hash = await zkInstance.methods.transferNoteToContract(
-                this.state.claimamount,
-                this.state.recipient,
-                senderBalalnce
-            ).send({
-                from: accounts[0]
+
+            const balalnceOfDai = await daiInstance.methods.balanceOf(accounts[0]).call();
+            console.log("Your Dai balance");
+            console.log(balalnceOfDai);
+
+            const claimReserve = await zkInstance.methods.noteMapping(accounts[0]).call()
+            console.log("Your private balance sent by someone, You can liquidiate it in your balance.")
+            console.log(claimReserve[2]);
+
+            this.setState({
+                daiBalance: balalnceOfDai,
+                claimBalance: claimReserve[2]
             });
-            console.log("Transaction hash: ", hash);
 
         } catch (error) {
-            console.log(error)
+            console.log(error);
+        }
+    };
+
+    ontransfer = async () => {
+        event.preventDefault();
+        try {
+            const accounts = await web3.eth.getAccounts();
+            
+            const daiInstance = await getERCContractInstance(web3);
+            const allowance = await daiInstance.methods.allowance(accounts[0], this.state.zkContractAddress).call();
+            const balance = await daiInstance.methods.balanceOf(accounts[0]).call();
+
+            const inWei = web3.utils.toWei(this.state.amount, "ether");
+            if(balance/1e18 >= inWei/1e18) {
+                if(parseInt(allowance) < web3.utils.toWei(this.state.amount, "ether")) {
+                    console.log("Approving Note balance..");
+                    const approvehash = await daiInstance.methods.approve(
+                        this.state.zkContractAddress, // ZkPrivateDai Contract address
+                        web3.utils.toWei(this.state.amount, "ether")
+                    ).send({
+                        from: accounts[0]
+                    });
+                    console.log("Approved: ", approvehash);
+                }
+                const afterallowance = await daiInstance.methods.allowance(accounts[0], this.state.zkContractAddress).call();
+                console.log("After approve note allowance is: ", afterallowance)
+
+                console.log("Transferrng Note...");
+                const senderBalalnceInWei = await daiInstance.methods.balanceOf(accounts[0]).call();
+                const senderBalanceInEther = web3.utils.fromWei(senderBalalnceInWei, "ether");
+                //0xE33f4C2306eFE9BF66a64A3c42408d2Fe1Cb890f
+
+                // Get ZkPrivateDai smart contract instance
+                const zkInstance = await getZkSnarkInstance(web3);
+
+                // Sender send Amount Privately and create note for transaction using this solidity function
+                const hash = await zkInstance.methods.transferNoteToContract(
+                    web3.utils.toWei(this.state.amount, "ether"),
+                    web3.utils.toWei(senderBalanceInEther, "ether"),
+                    this.state.recipient
+                ).send({
+                    from: accounts[0]
+                });
+                console.log("Transaction hash: ", hash);
+            } else {
+                alert("Insufficient Dai Balance!");
+            }
+        } catch (error) {
+            console.log(error);
         }
     };
 
@@ -54,46 +101,58 @@ class Index extends Component {
             const zkInstance = await getZkSnarkInstance(web3);
 
             const noteInfo = await zkInstance.methods.getNoteInfo(accounts[0]).call();
-            
-            // Calculate witness
-            const witnessparams = getWitness(
-                noteInfo[3], // sender
-                noteInfo[2], 
-                accounts[0], // receiver
-                this.state.claimamount
-            );
-            console.log(witnessparams)
+            console.log(noteInfo);
+            const senderAmount = web3.utils.fromWei(noteInfo[2], "ether");
+            console.log(senderAmount.toString());
 
-            // Generate Proof using witness params through zokrates library
-            // No need to generate proof using command line terminal 
-            // We have host the zokrates image in docker and automatically calculated when transaction happen 
-            // So We prioritize submissions that reduce onboarding friction and are usable to people unfamiliar with crypto
-            let generateProof;    
-            await Axios.get("http://localhost:3001/createproof?witness="+witnessparams, {})
-                .then((res) => {
-                    if (res.statusText == 'OK') {
-                        console.log("response:", res)
-                        generateProof = res
-                    } else {
-                        alert("error")
-                        console.log(res);
-                    }
-                })
-                .catch((err) => {
-                    alert("err")
-                    console.log(err);
-            });
+            if(noteInfo[0]){
+                if(noteInfo[1] == web3.utils.toWei(this.state.claimamount, "ether")){                    
+                    // Calculate witness
+                    const witnessparams = getWitness(
+                        noteInfo[3], // sender
+                        senderAmount.toString(),
+                        accounts[0], // receiver
+                        this.state.claimamount
+                    );
+                    console.log("Witness params to generate proof: ", witnessparams)
 
-            // 
-            const hash = await zkInstance.methods.claimNote(
-                generateProof.data.proof.a,
-                generateProof.data.proof.b,
-                generateProof.data.proof.c,
-                generateProof.data.inputs,
-                this.state.claimamount
-            ).send({
-                from: accounts[0]
-            });
+                    // // Generate Proof using witness params through zokrates library
+                    // // No need to generate proof using command line terminal 
+                    // // We have host the zokrates image in docker and automatically calculated when transaction happen 
+                    // // So We prioritize submissions that reduce onboarding friction and are usable to people unfamiliar with crypto
+                    let generateProof;    
+                    await Axios.get("http://localhost:3001/createproof?witness="+witnessparams, {})
+                        .then((proof) => {
+                            if (proof.statusText == 'OK') {
+                                console.log("Generate Proof Response from witness:", proof);
+                                generateProof = proof;
+                            } else {
+                                console.log(proof);
+                            }
+                        })
+                        .catch((err) => {
+                            console.log("Server Error: ", err);
+                    });
+
+                    // Claim amount privately 
+                    // By showing proof of the note and amount will liquidate if proof is correct
+                    const hash = await zkInstance.methods.claimNote(
+                        generateProof.data.proof.a,
+                        generateProof.data.proof.b,
+                        generateProof.data.proof.c,
+                        generateProof.data.inputs,
+                        web3.utils.toWei(this.state.claimamount, "ether")
+                    ).send({
+                        from: accounts[0]
+                    });
+                    this.checkBalalnce();
+                    console.log("ClaimeNote Hash: ", hash);
+                } else {
+                    alert("Claim amount is not valid!")
+                }
+            } else {
+                alert("Note is used!");
+            }
             
         } catch (error) {
             console.log(error);
@@ -155,6 +214,14 @@ class Index extends Component {
                             </Form.Field>
                             <Form.Field>
                                 <Button>claim amount</Button> 
+                            </Form.Field>                        
+                        </Form>
+                    </Grid.Row>
+
+                    <Grid.Row>
+                        <Form onSubmit={this.checkBalalnce}>
+                            <Form.Field>
+                                <Button>check balance</Button> 
                             </Form.Field>                        
                         </Form>
                     </Grid.Row>
